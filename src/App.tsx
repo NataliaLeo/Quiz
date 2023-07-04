4f694189f7096ccc33dafcab8b5547c942b43186
Наталья Л <housefuf@gmail.com>
2023-07-04 12:25:47 +0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Game } from "./components/Game";
+import "./index.scss";
+import "./question";
+
+const App = () => {
+	return (
+		<div className="App">
+			<Game />
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/Game.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import classes from "./Game.module.scss";
+import { Button } from "antd";
+
+export const Game = () => {
+	return <Button type="primary">Button</Button>;
+};
